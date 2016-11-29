45d4e714fe69709a1f368854d0bef3da21a4568f
Kacper Wikieł <phoebe@Kacpers-MacBook-Pro.local>
2016-11-29 12:38:22 +0100

    Numbers and IO examples

---
+++ Hello/src/InputOutput.java
@@ -0,0 +1,21 @@
+/**
+ * Created by phoebe on 29/11/16.
+ */
+import java.util.*;
+
+
+public class InputOutput {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+
+        System.out.println(a);
+
+
+    }
+
+}
---
+++ Hello/src/Numbers.java
@@ -0,0 +1,22 @@
+/**
+ * Created by phoebe on 29/11/16.
+ */
+public class Numbers {
+
+    public static void main(String[] args) {
+
+
+
+        // Typ liczbowy byte - 8 bitowa wartość, z znakiem
+        // Możliwe wartości: -128<= byte <= 127
+        byte fooByte = 100;
+
+        // Typ liczbowy short - 16 bitowa wartość, z znakiem
+        // Możliwe wartości: -32,768<= short <= 32,767
+        byte fooShort = 100;
+
+
+
+    }
+
+}
